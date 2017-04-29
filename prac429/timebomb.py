accd3e996340d69149d7c2ce69b38fad99a49f71
Michael Noronha <mnoronha@drowzee.cs.uchicago.edu>
2017-04-29 15:34:35 -0500

    two failed problems that we know why they fail but can't fix

--- prac429/timebomb.py
+++ prac429/timebomb.py
@@ -1,19 +1,51 @@
+import sys
 
 
 d = {}
 
-d[0] = {'***':{0,2,3,5,6,7,8,9},'  *':{1}}
-d[1] = {'* *':{0,4,8,9},        '  *':{2,3},         '*  ':{5,6}}
+d[0] = {'***':{0,2,3,5,6,7,8,9},'  *':{1},           '* *':{4}}
+d[1] = {'* *':{0,4,8,9},        '  *':{1,2,3,7},     '*  ':{5,6}}
 d[2] = {'***':{2,3,4,5,6,8,9},  '* *':{0},           '  *':{1,7}}
-d[3] = {'* *':{0,6,8},          '  *':{1,3,4,5,7,9}}
-d[4] = {'***':{0,2,3,5,6,8,9},  '  *':{1,4}}
+d[3] = {'* *':{0,6,8},          '  *':{1,3,4,5,7,9}, '*  ':{2}}
+d[4] = {'***':{0,2,3,5,6,8,9},  '  *':{1,4,7}}
 
 lines = []
-for i in range(5):
-	lines.append(input())
+try:
+    for i in range(5):
+        lines.append(input())
+except:
+    print('BOOM!!')
+    sys.exit()
 
 for l in lines:
-    print(l)
+    if len(l) != len(lines[0]):
+       print('BOOM!!')
+       sys.exit()
 
-[{0,1,2,3,4,5,6,7,8,9} for i in range((len(lines[0])+1)/4)]
+sets = [{0,1,2,3,4,5,6,7,8,9} for i in range(0, len(lines[0]),4)]
+
+for i in range(5):
+    for j in range(len(sets)):
+        lb = j*4
+        rb = lb + 3
+
+        keyy = lines[i][lb:rb] # 3 digit keys
+        if keyy in d[i]:
+            sets[j] &= d[i][keyy]
+        else:
+            print('BOOM!!')
+            sys.exit()
+
+final = ''
+for i, s in enumerate(sets):
+    #  print(i)
+    final += str(s.pop())
+
+#print(final)
+
+if int(final) %6 == 0:
+    print('BEER!!')
+else:
+    print('BOOM!!')
+
 
